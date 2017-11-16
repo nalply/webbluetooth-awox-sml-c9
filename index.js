e8ea0b581c6a625a980e8bfa5abaad1e871dd72e
Daniel Ly <daniel.ly@lively5.ch>
2017-11-16 11:50:11 +0100

    Added an "advanced" section to send raw commands to the bulb. The bulb sometimes shows erratic behaviour. Perhaps the codes I am using aren't quite correct. For example I can change the color the second time only after sending the off and on commands. Another example is, when I submit the c9Command() from DevTools programmatically then the bulb resets to the white mode, and inserting a wait of 1 second between the commands (debounce) didn't help. WTF.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 window.store = { hue: 0 }
 window.addEventListener('load', async function() {
   onclick('#connection', connection)
-  onclick('#on', ev => command('on'))
-  onclick('#off', ev => command('off'))
+  onclick('#on', ev => c9Command('on'))
+  onclick('#off', ev => c9Command('off'))
   onchange('#lightness', ev => paintColorsWheel(ev.target.value))
   onmousemove('#panel', ev => mouseoverPanel(ev.offsetX, ev.offsetY))
   onclick('#panel', ev => clickPanel(ev.offsetX, ev.offsetY))
+  onchange('#command', ev => {
+    const command = localStorage.commandSelectedOption = ev.target.value
+    showParams(command)
+  })
+  onclick('.go', ev => go(ev.target))
 
   elem('#colorsUI', el => {
     const style = getComputedStyle(el)
@@ -14,9 +19,40 @@ window.addEventListener('load', async function() {
     store.ctx = el.getContext('2d')
   })
 
+  if (!localStorage.commandSelectedOption)
+    localStorage.commandSelectedOption = 'rgb'
   updateView()
   elem('#panel', el => el.dataset.show = true)
 })
+
+
+async function connection() {
+  if (store.gatt) {
+    bluetoothDisconnect()
+    delete store.gatt
+    delete store.light
+  }
+  else {
+    await bluetoothConnect()
+  }
+  updateView()
+}
+
+
+async function go(el) {
+  const command = elem('#command')[0].value
+  switch (command) {
+    case 'brightness': {
+      const paramBrightness = +elem('#param_brightness')[0].value
+      await c9Command('brightness', paramBrightness)
+    }; break
+    case 'rgb': {
+      const paramRGB = getRGB(elem('#param_rgb')[0].value)
+      await c9Command('rgb', ...paramRGB)
+    }; break
+    default: console.error('Unknown command', command)
+  }
+}
 
 
 function elem(sel, f) {
@@ -48,72 +84,14 @@ function updateView() {
   elem('#panel', el => el.dataset.show = connected)
   elem('#connection', el => el.innerText = connected ? 'disconnect' : 'connect')
   paintColorsWheel(elem('#lightness')[0].value)
+  elem('#command', el => el.value = localStorage.commandSelectedOption)
+  showParams(localStorage.commandSelectedOption)
 }
 
 
-async function connection() {
-  if (store.gatt) {
-    store.gatt.disconnect()
-    delete store.gatt
-    delete store.light
-  }
-  else {
-    const device = await navigator.bluetooth.requestDevice({
-      filters: [{ name: '' }, { namePrefix: 'SML' }],
-      optionalServices: [ 0x180a, 0xfff0 ]
-    })
-    store.gatt = await device.gatt.connect()
-    const service = await store.gatt.getPrimaryService(0xfff0)
-    store.light = await service.getCharacteristic(0xfff1)
-    if (!store.light) throw new Error('characteristic not ready')
-  }
-  updateView()
-}
-
-
-async function command(what, ...args) {
-  const head = [0xAA, 0x0A, 0xFC, 0x3A, 0x86, 0x01]
-  const on = [0x0A, 0x01, 0x01, 0x00, 0x28]
-  const off = [0x0A, 0x01, 0x00, 0x01, 0x28]
-  const rgb1 = [0x0D, 0x06]
-  const rgb2 = [0x20, 0x30]
-
-
-  switch (what) {
-    case 'on': send(onCode()); break
-    case 'off': send(offCode()); break
-    case 'rgb': send(rgbCode(...args), 1); break; 
-    default: console.error('Unknown command', what)
-  }
-
-  async function send(code) {
-    console.debug('sending', codeToString(code))
-    await store.light.writeValue(new Uint8Array(code))
-    console.debug('sent!')
-  }
-
-  function onCode() { return [...head, ...on, 0x0d] }
-
-  function offCode() { return [...head, ...off, 0x0d] }
-  
-  function rgbCode(r, g, b) {
-    console.log(`rgb(${r},${g},${b})`)
-    const code = [...head, ...rgb1,
-      0x01, r & 0xff, g & 0xff, b & 0xff, 
-      ...rgb2, 137]
-    code.push(checksum(code))
-    code.push(0x0d)
-    return code
-  }
-
-  // see dragouf/aws-smartlight lib.js:129 https://goo.gl/mxMRxu
-  function checksum(code) {
-    return (code.slice(1).reduce((a, b) => a + b) + 85) & 0xFF
-  }
-
-  function codeToString(code) {
-    return code.map(_ => _.toString(16).padStart(2, '0')).join(' ')
-  }
+function showParams(command) {
+  elem('.params', el => el.dataset.show = false)
+  elem('#cmd_' + command, el => el.dataset.show = true)
 }
 
 
@@ -124,7 +102,7 @@ function paintColorsWheel(lightness) {
 
   ctx.lineWidth = 50
   for (let i = 0; i < 360; i += d) {
-    ctx.strokeStyle = color(i, lightness)
+    ctx.strokeStyle = hsl(i, lightness)
     ctx.beginPath()
     ctx.arc(100, 100, 70, i * d2r , (i + d + 0.5) * d2r)
     ctx.stroke()
@@ -134,7 +112,7 @@ function paintColorsWheel(lightness) {
 
 function paintCentralColor(hue, lightness) {
   const ctx = store.ctx
-  ctx.fillStyle = color(hue, lightness)
+  ctx.fillStyle = hsl(hue, lightness)
   ctx.fillRect(75, 75, 50, 50)
   ctx.lineWidth = 1
   ctx.strokeStyle = 'grey'
@@ -142,7 +120,7 @@ function paintCentralColor(hue, lightness) {
 }
 
 
-function color(hue, lightness) {
+function hsl(hue, lightness) {
   return `hsl(${hue},100%,${lightness}%)`
 }
 
@@ -163,7 +141,7 @@ function clickPanel(x, y) {
   const lightness = elem('#lightness')[0].value
   paintCentralColor(hue, elem)
 
-  command('rgb', ...getRGB(hue, lightness))
+  c9Command('rgb', ...getRGB(hsl(hue, lightness)))
 }
 
 
@@ -178,12 +156,97 @@ function getHue(x, y) {
 }
 
 
-function getRGB(hue, lightness) {
+function getRGB(cssColor) {
   const colorConverterElement = elem('#cconv')[0]
-  colorConverterElement.style.color = color(hue, lightness)
+  colorConverterElement.style.color = cssColor
   return getComputedStyle(colorConverterElement).color
     .match(/^rgb\((\d+), *(\d+), *(\d+)\)$/)
     .slice(1).map(x => +x)
+}
+
+
+
+
+// --- Web Bluetooth functions ---
+
+async function bluetoothConnect() {
+  const device = await navigator.bluetooth.requestDevice({
+    filters: [{ name: '' }, { namePrefix: 'SML' }],
+    optionalServices: [ 0x180a, 0xfff0 ]
+  })
+  store.gatt = await device.gatt.connect()
+  const service = await store.gatt.getPrimaryService(0xfff0)
+  store.light = await service.getCharacteristic(0xfff1)
+  if (!store.light) throw new Error('characteristic not ready')
+}
+
+
+async function bluetoothDisconnect() {
+  store.gatt.disconnect()
+}
+
+
+async function c9Command(what, ...args) {
+  const headBytes = [0xAA, 0x0A, 0xFC, 0x3A, 0x86, 0x01]
+  const onBytes = [0x0A, 0x01, 0x01, 0x00, 0x28]
+  const offBytes = [0x0A, 0x01, 0x00, 0x01, 0x28]
+  const brightnessBytes = [0x0C, 0x01]
+  const rgb1Bytes = [0x0D, 0x06]
+  const rgb2Bytes = [0x20, 0x30]
+  const lastByte = 0x0d
+
+  switch (what) {
+    case 'on': await send(onCode()); break
+    case 'off': await send(offCode()); break
+    case 'rgb': await send(rgbCode(...args)); break
+    case 'brightness': await send(brightnessCode(...args)); break
+    default: console.error('Unknown command', what)
+  }
+
+  async function send(code) {
+    console.debug('sending', codeToString(code))
+    await store.light.writeValue(new Uint8Array(code))
+    console.debug('sent!')
+  }
+
+  function onCode() { return [...headBytes, ...onBytes, lastByte] }
+
+  function offCode() { return [...headBytes, ...offBytes, lastByte] }
+  
+  function rgbCode(r, g, b, slow) {
+    console.log(`rgb(${r},${g},${b})`)
+    const code = [
+      ...headBytes, ...rgb1Bytes,
+      toByte(slow), toByte(r), toByte(g), toByte(b), 
+      ...rgb2Bytes, 137,
+    ]
+    code.push(checksum(code))
+    code.push(lastByte)
+    return code
+  }
+
+  function brightnessCode(brightness) {
+    const code = [
+      ...headBytes, ...brightnessBytes, toByte(9 * brightness + 2),
+    ]
+    code.push(checksum(code))
+    code.push(lastByte)
+    return code
+  }
+
+  // see dragouf/aws-smartlight lib.js:129 https://goo.gl/mxMRxu
+  function checksum(code) {
+    return (code.slice(1).reduce((a, b) => a + b) + 85) & 0xFF
+  }
+
+  function codeToString(code) {
+    return code.map(_ => _.toString(16).padStart(2, '0')).join(' ')
+  }
+
+  function toByte(x) {
+    x = x | 0
+    return x < 0 ? 0 : x > 255 ? 255 : x
+  }
 }
 
 // 0x2a24 SML-c9
